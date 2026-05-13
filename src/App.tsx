dc4d9f12bb4da6c0c2d7a7b533ffd56a27453613
zuiai <zuiai@qunhemail.com>
2026-05-13 15:30:35 +0800

    Mermaid

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { HomePage } from "@/pages/HomePage";
@@ -11,7 +11,7 @@ import { ResortDetailPage } from "@/pages/ResortDetailPage";
 
 export default function App() {
   return (
-    <Router basename="/Snowboarding">
+    <Router>
       <Header />
       <main className="min-h-screen">
         <Routes>
